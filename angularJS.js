d2cd3a6c59b332eeb52bb001306e7689ee5a98a7
siva92 <siva@obelisksolutions.in>
2014-10-07 15:58:24 +0530

    First Commit

---
+++ angularJS.js
@@ -0,0 +1,67 @@
+
+var app = angular.module('app',[]);
+
+app.controller('BodyCtrl',['$scope',
+  function($scope){
+
+    $scope.clear = function(){
+      $scope.searchString = "";
+    };
+
+    $scope.submit = function(){
+      document.getElementById("print").innerHTML="";
+
+      if(checkValidation() === true  ){
+        readTextFile();
+      }
+    };
+
+
+    var checkValidation =function(){
+      if(document.getElementById("search").value.length === 0 ){
+        alert("Please Enter the Search String");
+        return false;
+      }
+
+      return true;
+    }
+
+    
+    var readTextFile = function(){
+      var rawFile = new XMLHttpRequest();
+      var file = "strings.txt";
+      rawFile.open("GET",file,false);
+
+      var search = document.getElementById("search").value.trim();
+      var re = new RegExp(search,'gi');
+      var result ;
+
+
+      rawFile.onreadystatechange = function(){
+       if(rawFile.readyState === 4 ){
+        if(rawFile.status === 200 || rawFile.status === 0 ){
+          var allText = rawFile.responseText;
+          var lines = allText.split('\n');
+          for(var i = 0; i < lines.length; i++){
+            console.log(lines[i]);
+            result = lines[i].match(re);
+
+            console.log(result);
+            if(result != null){
+             document.getElementById("print").innerHTML += lines[i] + "<br>";
+           }
+
+
+         }
+         
+         
+       }
+     }
+
+     
+   }
+
+   rawFile.send(null);
+ }
+
+}]);
